f9eec8e4b23a9b120ad3b7489d43a0d074fc1b4b
Suleman Kadiri <skadiri94@outlook.com>
2021-04-05 19:37:51 +0100

    updated commit

---
+++ src/db/db-connection.js
@@ -0,0 +1,67 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mariadb = require("mariadb/callback");
+
+class DBConnection {
+  constructor() {
+    this.pool = mariadb.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      connectionLimit: 5,
+    });
+
+    this.checkConnection();
+  }
+
+  checkConnection() {
+    this.pool.getConnection((err, connection) => {
+      if (err) {
+        if (err.code === "PROTOCOL_CONNECTION_LOST") {
+          console.error("Database connection was closed.");
+        }
+        if (err.code === "ER_CON_COUNT_ERROR") {
+          console.error("Database has too many connections.");
+        }
+        if (err.code === "ECONNREFUSED") {
+          console.error("Database connection was refused.");
+        }
+      }
+      if (connection) connection.release();
+
+      return;
+    });
+  }
+
+  query = async (sql, values) => {
+    return new Promise((resolve, reject) => {
+      const callback = (error, result) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(result);
+      };
+      // execute will internally call prepare and query
+      this.pool.query(sql, values, callback);
+      //   this.pool.end();
+    }).catch((err) => {
+      const mysqlErrorList = Object.keys(HttpStatusCodes);
+      // convert mysql errors which in the mysqlErrorList list to http status code
+      err.status = mysqlErrorList.includes(err.code)
+        ? HttpStatusCodes[err.code]
+        : err.status;
+
+      throw err;
+    });
+  };
+}
+
+// like ENUM
+const HttpStatusCodes = Object.freeze({
+  ER_TRUNCATED_WRONG_VALUE_FOR_FIELD: 422,
+  ER_DUP_ENTRY: 409,
+});
+
+module.exports = new DBConnection().query;

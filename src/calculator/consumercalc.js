9b878fb2de17b6da60459ec3e96de139281260fe
SuperOmario <omarsallamala@gmail.com>
2021-06-10 12:21:36 +0100

    Fixed carbon to be yearly

--- src/calculator/consumercalc.js
+++ src/calculator/consumercalc.js
@@ -4,11 +4,11 @@ const calculateServicesCO2 = (serviceValues) => {
   const { office, clothing, phone, tv, internet } = serviceValues;
   console.log("the multipler for service is : ", multipliers);
   const servicesCO2 =
-    tv * multipliers.tvCO2perHour +
-    phone * multipliers.callCO2perMinute +
-    internet * multipliers.internetCO2perGB +
-    office * multipliers.paperCO2perEuro +
-    clothing * multipliers.clothingCO2perEuro;
+    ((tv * multipliers.tvCO2perHour) +
+    (phone * multipliers.callCO2perMinute) +
+    (internet * multipliers.internetCO2perGB) +
+    (office * multipliers.paperCO2perEuro) +
+    (clothing * multipliers.clothingCO2perEuro)) * 12;
   return servicesCO2;
 };
 

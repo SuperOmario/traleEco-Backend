dce1301471b36bfd89847a0db2b31a8f7d4866f6
SuperOmario <omarsallamala@gmail.com>
2021-06-10 12:27:59 +0100

    Convert CO2 to US tons at the end

--- src/calculator/calculator.js
+++ src/calculator/calculator.js
@@ -9,10 +9,10 @@ const calculateCarbonFootprint = (
   serviceValues,
   transportValues
 ) => {
-  const foodCO2 = calculateFoodCO2(foodValue);
-  const homeCO2 = calculateHomeCO2(homeValues);
-  const transportCO2 = calculateTransportCO2(transportValues);
-  const serviceCO2 = calculateServicesCO2(serviceValues);
+  const foodCO2 = calculateFoodCO2(foodValue) * 0.0011;
+  const homeCO2 = calculateHomeCO2(homeValues) * 0.0011;
+  const transportCO2 = calculateTransportCO2(transportValues) * 0.0011;
+  const serviceCO2 = calculateServicesCO2(serviceValues) * 0.0011;
   const totalCO2 = foodCO2 + homeCO2 + transportCO2 + serviceCO2;
   const CarbonFootprint = {
     FoodCarbon: foodCO2,

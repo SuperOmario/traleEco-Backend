8d5e119fdb8af37d848aeaaa90d3196be2a8f957
SuperOmario <omarsallamala@gmail.com>
2021-06-10 12:49:10 +0100

    Tried to fix Home Carbon

--- src/models/questionnaire.model.js
+++ src/models/questionnaire.model.js
@@ -118,7 +118,7 @@ class QModel {
     plastic,
     glass,
     paper,
-    recycleCans,
+    cans,
     userId,
   }) => {
     const sql = `INSERT INTO Home
@@ -137,7 +137,7 @@ class QModel {
       plastic,
       glass,
       paper,
-      recycleCans,
+      cans,
       userId,
     ]);
     const affectedRows = result ? result.affectedRows : 0;

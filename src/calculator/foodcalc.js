f469a1321bcdc806d9787a2db8e93b91db836331
SuperOmario <omarsallamala@gmail.com>
2021-06-14 12:20:52 +0100

    Constraints for food calculator + extended metrics for transport calc

--- src/calculator/foodcalc.js
+++ src/calculator/foodcalc.js
@@ -3,7 +3,7 @@ const Mutlipliers = require("../utils/foodMultipliers.utils");
 //Calculates CO2 emissions from food by the tonne per year
 const calculateFoodCO2 = (foodValue) => {
   const { beef, pork, chicken, fish, dairy, local, homegrown } = foodValue;
-
+  console.log(beef, pork, chicken, fish, dairy, local, homegrown);
   const meat = beef + pork + chicken;
 
   let FoodCO2 = 0;
@@ -12,10 +12,25 @@ const calculateFoodCO2 = (foodValue) => {
   if (meat + fish + dairy == 0) {
     FoodCO2 = Mutlipliers.veganMultiplier * 7;
     FoodCO2 *= 365;
+
+    // if local food is purchased this offsets the CO2 produced by about 6%
+  if (local >= 1) {
+    localOffset = (FoodCO2 * 0.06).toFixed(2);
+  }
+  // //percentage of food which is homegrown offsets CO2 by a maximum of 6%
+  if (homegrown > 0) {
+    let hgOffsetPercentage = homegrown * 0.06;
+    if (hgOffsetPercentage > 0.06) {
+      hgOffsetPercentage = 0.06
+    }
+    homegrownOffset = (FoodCO2 * hgOffsetPercentage).toFixed(2);
+  }
+  FoodCO2 -= localOffset;
+  FoodCO2 -= homegrownOffset;
+
     return FoodCO2.toFixed(2);
   }
   // //if not vegan, calculate based on days consuming fish and other meats
-  // //maybe clean up the figures. get them from a utils file
   let meatMultiplier = 0;
 
   const fishMultiplier = Mutlipliers.fishMultiplier;
@@ -32,12 +47,20 @@ const calculateFoodCO2 = (foodValue) => {
     case meat == 0:
       break;
   }
-  const vegDays = 7 - (meat + fish);
-  console.log("Got here 1 :", FoodCO2, "VegDays is : ", vegDays);
-  FoodCO2 = (
-    (meat * meatMultiplier + fish * fishMultiplier + vegDays * vegMultiplier) *
-    (365 / 7)
-  ).toFixed(2);
+  if (7 - (meat + fish) > 0) {
+    let vegDays = 7 - (meat + fish);
+    FoodCO2 = (
+      (meat * meatMultiplier + fish * fishMultiplier + vegDays * vegMultiplier) *
+      (365 / 7)
+    ).toFixed(2);
+    console.log(vegDays)
+  } else {
+    FoodCO2 = (
+      (meat * meatMultiplier + fish * fishMultiplier) *
+      (365 / 7)
+    ).toFixed(2);
+  }
+  
   FoodCO2 = calculateOffset(FoodCO2, local, homegrown);
   return FoodCO2;
 };
@@ -53,11 +76,13 @@ const calculateOffset = (FoodCO2, local, homegrown) => {
   // //percentage of food which is homegrown offsets CO2 by a maximum of 6%
   if (homegrown > 0) {
     let hgOffsetPercentage = homegrown * 0.06;
+    if (hgOffsetPercentage > 0.06) {
+      hgOffsetPercentage = 0.06
+    }
     homegrownOffset = (FoodCO2 * hgOffsetPercentage).toFixed(2);
   }
-  FoodCO2 = FoodCO2 - localOffset;
+  FoodCO2 -= localOffset;
   FoodCO2 -= homegrownOffset;
-  FoodCO2 = FoodCO2 - localOffset;
   return FoodCO2;
 };
 
--- src/calculator/transportcalc.js
+++ src/calculator/transportcalc.js
@@ -1,13 +1,46 @@
 const transportMultipliers = require("../utils/transportMultipliers.utils");
 
 const calculateTransportCO2 = (transportValue) => {
-  const { vehicle, milage, passengers } = transportValue;
+  const { vehicle, fuel, milage, passengers } = transportValue;
   let transportCO2 = 0;
-  if (vehicle == "Car") {
-    transportCO2 = transportMultipliers.AvgC02PerKM * milage;
-    transportCO2 = transportCO2 / passengers;
+  switch (vehicle) {
+    case "Car" :
+      console.log("Car");
+      switch (fuel) {
+        case "Petrol" :
+          transportCO2 = transportMultipliers.CarPetrolAvgC02PerKM * milage;
+          break;
+        case "Diesel" :
+          transportCO2 = transportMultipliers.CarDieselAvgC02PerKM * milage;
+          break;
+        case "Hybrid" :
+          transportCO2 = transportMultipliers.CarHybridAvgC02PerKM * milage;
+          break;
+      }
+        transportCO2 = transportCO2 / passengers;
+        return transportCO2;;
+    case "Van" :
+      console.log("Van");
+      switch (fuel) {
+        case "Petrol":
+          transportCO2 = transportMultipliers.VanPetrolAvgC02PerKM * milage;
+          break;
+        case "Diesel" :
+          transportCO2 = transportMultipliers.VanDieselAvgC02PerKM * milage;
+          break;
+      }
+      transportCO2 = transportCO2 / passengers;
+      return transportCO2;;
+    case "Motorcycle":
+      console.log("Motorcycle");
+      switch (fuel) {
+        case "Petrol":
+          transportCO2 = transportMultipliers.MotorcyclePetrolAvgC02PerKM * milage;
+          break;
+      } 
+      transportCO2 = transportCO2 / passengers;
+      return transportCO2;;
   }
-  return transportCO2;
 };
 
 module.exports = calculateTransportCO2;
